8bd3dd898efa7be8caeb24e041634406699c26db
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-15 17:41:32 +0500

    Start question no 1 and initialize function with n argument

---
+++ script.js
@@ -0,0 +1,7 @@
+//Question: 01
+//Write a function called sumRange. It will take a number and return 
+// the sum of all numbers from 1 up to the number passed in.
+// Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
+function sumRange(n) {
+    
+}
