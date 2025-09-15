a19c802e5386fb9607b6f84cf5fa69b28a17184f
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-15 17:50:14 +0500

    Now to question no 2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //Question: 01
 //Write a function called sumRange. It will take a number and return 
 // the sum of all numbers from 1 up to the number passed in.
-// Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
 function sumRange(n, total = 0) {
     if(n <= 0) {
         return total;
@@ -9,3 +8,6 @@ function sumRange(n, total = 0) {
         return sumRange(n - 1, total + n);
     }
 }
+//Question 2: Power function
+//Write a function called power which takes in a base and an exponent. 
+// If the exponent is 0, return 1.
