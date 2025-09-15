2bb6bdb4771dea969f100de61fe7cfe50184ba54
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-15 17:43:38 +0500

    Write the base case if n is less than or equal to zero then return the total variable

--- script.js
+++ script.js
@@ -3,5 +3,7 @@
 // the sum of all numbers from 1 up to the number passed in.
 // Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
 function sumRange(n, total = 0) {
-    
+    if(n <= 0) {
+        return total;
+    } 
 }
