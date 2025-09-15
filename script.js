d0083dbb180842f392b4783004523d5ac9e78be8
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-15 17:42:44 +0500

    As introduction of sum variable within the function will only be available for that instance of recursion so we introduce another argument as total with initial value as 0

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 //Write a function called sumRange. It will take a number and return 
 // the sum of all numbers from 1 up to the number passed in.
 // Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
-function sumRange(n) {
+function sumRange(n, total = 0) {
     
 }
