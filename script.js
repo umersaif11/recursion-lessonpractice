fb44d71121bb1513373309068f19bfef940b1f06
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-16 14:22:37 +0500

    Now onto the question 4

--- script.js
+++ script.js
@@ -27,3 +27,8 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }
+//Question 4: Check all values in an array
+//Write a function called all which accepts an array and a 
+// callback and returns true if every value in the array returns
+//  true when passed as parameter to the callback function
+
