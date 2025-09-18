9520508d80aefd2df995925abe217d609dc81146
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-18 12:20:34 +0500

    Introduce function with array as an argument

--- script.js
+++ script.js
@@ -76,7 +76,20 @@ function contains(object, value) {
 // Question 7: Parse a multi-dimensional array
 // Given a multi-dimensional integer array, return 
 // the total number of integers stored inside this array
-function totalIntegers(array, i = 0){
+//var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+function totalIntegers(array, i = 0, total = 0){
+    if(!Array.isArray(array[i]) && Number.isInteger(array[i])){
+        total++;
+        return total;
+    } else if(Array.isArray(array[i])){
+        return totalIntegers(array[i], i + 1, total);
+    }
+}
+function totalnum(array){
+    let total = 0;
+    
     
 }
+var arr = [0, [[5], 3], 2, ['foo'], [], [4, [5, 6]],8,[7]];
+console.log(totalnum(arr));
 
