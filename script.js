54aaa453ad400b53bbf0be9b1c46fb6da9d0e8eb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-18 14:41:58 +0500

    Introduce the function with arguments of number, times, i=0 and empty array

--- script.js
+++ script.js
@@ -112,5 +112,13 @@ function SumSquares(array){
     }
     return total;
 }
+//Question 9:
+//The function should return an array containing repetitions
+//of the number argument. For instance, replicate(3, 5) should
+//return [5,5,5]. If the times argument is negative, return an empty array.
+function replicate(times, number, i = 0, array = []){
+   
+    
+    
 
-
+}
