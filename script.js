a2f51debd5d1908876d2e92d9e2f9f0ad12f5491
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-09-18 14:42:33 +0500

    If times is less than 0 then return empty array

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function SumSquares(array){
 //of the number argument. For instance, replicate(3, 5) should
 //return [5,5,5]. If the times argument is negative, return an empty array.
 function replicate(times, number, i = 0, array = []){
-   
+    if(times < 0) return [];
     
     
 
